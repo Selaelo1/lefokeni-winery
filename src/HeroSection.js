bf168bec0b082932b0b32c953212cb497de399d3
Selaelo1 <selaelolanga10@gmail.com>
2024-11-07 16:45:53 +0200

    Create HeroSection.js

---
+++ src/HeroSection.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import './HeroSection.css';
+
+const HeroSection = () => {
+  return (
+    <section className="hero">
+      <h1 className="hero-title">Welcome to Lefokeni Winery</h1>
+    </section>
+  );
+};
+
+export default HeroSection;
+
+
+
