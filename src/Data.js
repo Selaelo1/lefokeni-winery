c19c215af40aaffc2352c66e9f290135d3a92d9c
Selaelo1 <selaelolanga10@gmail.com>
2024-11-07 16:45:27 +0200

    Create Data.js

---
+++ src/Data.js
@@ -0,0 +1,62 @@
+
+
+export const wines = [
+  {
+    id: 1,
+    name: 'Nkgari Chenin Blanc',
+    description: 'A rich, golden wine with intense aromas of ghee butter, popcorn, and bruised apple. Its complex palate features oak, beeswax, and toasted sugar, pairing perfectly with roasted chicken, lobster, or aged cheeses. A robust, high-quality wine ready to drink now or age, offering exceptional value.',
+    image: '../images/chenin-blanc.jpg',
+    price: 'R330',
+  },
+  {
+    id: 2,
+    name: 'Mapoteng Sweet Rose',
+    description: "Lefokeni Winery's masterpiece from Rawsonville's premier vineyards, boasts a rich, velvety texture and balanced flavors. Perfectly paired with grilled favorites, roasted potatoes, and sweetcorn, this Sweet Rosé offers an unforgettable taste experience. Savor the taste of Mapoteng Sweet Rosé.",
+    image: '../images/sweet-rose.jpg',
+    price: 'R360',
+  },
+  {
+    id: 3,
+    name: 'Maidi Merlot',
+    description: 'A refreshing standout in a crowd of over-oaked and over-ripe alternatives. This mountain-grown Merlot shines with intense, lengthy flavors, gorgeous purity of fruit, and a mineral-rich backbone. A superb effort, it pairs beautifully with a variety of holiday dishes, from roasted meats to rich cheeses, making it the perfect centerpiece for your gatherings.',
+    image: '../images/merlot.jpg',
+    price: 'R400',
+  },
+];
+
+export const events = [
+  {
+    id: 1,
+    name: 'Wine Tasting',
+    date: '2024-10-20',
+    venue: 'Lefokeni Winery',
+    time: '6:00 PM',
+    cost: '$50',
+    description: 'Join us for an evening of wine tasting and learning.',
+    video: '../images/event1.mp4',
+  },
+  {
+    id: 2,
+    name: 'Vineyard Tour',
+    date: '2024-11-15',
+    venue: 'Lefokeni Vineyards',
+    time: '10:00 AM',
+    cost: '$30',
+    description: 'Explore our beautiful vineyards and learn about our winemaking process.',
+    image: '../images/harvest-festival.jpg',
+  },
+  {
+    id: 3,
+    name: 'Game Night',
+    date: '2024-12-10',
+    venue: 'Lefokeni Winery',
+    time: '7:00 PM',
+    cost: '$75',
+    description: 'Indulge in a wine and cheese pairing experience.',
+    image: '../images/game-night.jpg',
+  },
+];
+
+
+
+
