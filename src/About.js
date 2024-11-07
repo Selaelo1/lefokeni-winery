b8ba65b46279a6b9b60e2f492d1352365bc15347
Selaelo1 <selaelolanga10@gmail.com>
2024-11-07 16:45:11 +0200

    Create About.js

---
+++ src/About.js
@@ -0,0 +1,29 @@
+
+import React from 'react';
+import './About.css';
+
+const About = () => {
+  return (
+   
+      <div>
+        <h2 >About Us</h2>
+        <p className="about-text">
+          Est.2021 & family operated by the M & M clan Thriving in making fine wines where tradition meets innovation & we are a acclaimed winery maker located in the outside beautiful mountains of Makgoebaskloof in Limpopo,from the vineyard soil to the last scrape of the fermentation tank an our wines are unique crafted in the vineyard and the cellar.
+        </p>
+        <p className="about-text">
+          Our winemaking team has years of experience crafting wines that showcase the best of our vineyards.
+        </p>
+      </div>
+  
+  );
+};
+
+export default About;
+
+
+
+
+
+
+
+
